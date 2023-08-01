029b4943a9fa18ddd1029dc4e24801ab4546b063
jtdoaks <136029371+jtdoaks@users.noreply.github.com>
2023-07-31 20:53:05 -0400

    high scores viewed

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let timer = 100;
-
-
+let timerInterval;
 let quizDiv = document.querySelector("#quiz");
 let beginDiv = document.querySelector("#begin");
 let cat = document.querySelector("#question");
 
 let timerPlaceholder = document.querySelector("#timer");
-
+let inputs = document.querySelector("#inputs");
 let answerButton1 = document.querySelector("#answer1");
 let answerButton2 = document.querySelector("#answer2");
 let answerButton3 = document.querySelector("#answer3");
 let answerButton4 = document.querySelector("#answer4");
-let highScores = JSON.parse(localStorage.getItem("high scores")) || [];
 let finished = document.querySelector("#finished");
+let finalScoreEl = document.querySelector("#finalScore");
+let finalScore = 0;
+let highScores = JSON.parse(localStorage.getItem("high-scores")) || [];
 
 let questions = [{ question: "Which pokemon does Ash get to start pokemon?", answers: ["charmander", "squirtle", "bulbasaur", "pikachu"], correctAnswer: "pikachu" },
 
@@ -32,43 +33,38 @@ let questions = [{ question: "Which pokemon does Ash get to start pokemon?", ans
 ];
 
 
-let currentQuestion = 0
+let currentQuestion = 0;
 renderQuestion();
 
 
 function renderQuestion() {
+    if (currentQuestion >= questions.length) {
+       clearInterval(timerInterval);
+        console.log("game over")
+        finishedEl.classList.toggle("hide")
+        quizDiv.classList.toggle("hide")
+        inputs.classList.toggle("hide")
+        finalScoreEl.classList.toggle("hide")
+        finalScoreEl.textContent = timer;
+        return;
+    };
 
     cat.textContent = questions[currentQuestion].question
     answerButton1.textContent = questions[currentQuestion].answers[0];
     answerButton2.textContent = questions[currentQuestion].answers[1];
     answerButton3.textContent = questions[currentQuestion].answers[2];
     answerButton4.textContent = questions[currentQuestion].answers[3];
-    if (currentQuestion === questions.length - 1) {
-       
-        console.log("game over")
-        finishedEl.classList.toggle("hide")
-        quizDiv.classList.toggle("hide")
-        inputs.classList.toggle("hide")
-    };
 }
-
-
 
 
 let startButtonEl = document.querySelector("#start")
 
 startButtonEl.addEventListener("click", startQuiz);
 
-function myFunction() {
-    startButtonEl.innerHTML = "Hello World";
-}
-
-
-
 
 function startQuiz() {
     document.querySelector("#timerEl").textContent = timer;
-    let timerInterval = setInterval(function () {
+    timerInterval = setInterval(function () {
         timer--
         document.querySelector("#timerEl").textContent = timer
         if (timer <= 0) {
@@ -83,14 +79,14 @@ function startQuiz() {
 }
 
 
-
-
 quizDiv.addEventListener("click", function (event) {
 
     if (event.target.matches("button")) {
 
         if (event.target.innerText === questions[currentQuestion].correctAnswer) {
-            console.log("You got it Right!")
+            console.log("You got it Right!");
+            finalScore ++;
+           
         } else {
             console.log("you got it wrong");
             timer -= 5;
@@ -102,6 +98,20 @@ quizDiv.addEventListener("click", function (event) {
 
 })
 
-
+inputs.addEventListener("submit", function (event){
+    event.preventDefault();
+    let userInitials = document.querySelector("#userInitials").value;
+    let user = {initials: userInitials, score: timer}
+    highScores.push(user);
+    localStorage.setItem("high-scores", JSON.stringify(highScores))
+    inputs.classList.toggle("hide");
+    leaderboard.classList.toggle("hide");
+    for (let i = 0; i < highScores.length; i++) {
+        const element = highScores[i];
+        let li = document.createElement("li");
+        li.textContent = "player: " + element.initials + " -- score: " + element.score;
+        leaderboard.append(li);
+    }
+})
 // page that says quiz is Over. highscores, inititials.
 // type inititals in and see their final score.
